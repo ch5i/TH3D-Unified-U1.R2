46bf65f603b4abf5610ae7da895fc4be5207af1f
Thomas <ch5i@users.noreply.github.com>
2020-05-02 01:03:09 +0200

    limited bed size X/Y if BLTouch is used

--- TH3DUF_R2/Configuration_backend.h
+++ TH3DUF_R2/Configuration_backend.h
@@ -2325,9 +2325,15 @@
   #endif
   
   #if ENABLED(ENDER5)
-    #define X_BED_SIZE 235
-    #define Y_BED_SIZE 235
-    #define Z_MAX_POS 300
+	   #if ENABLED(BLTOUCH)
+		    #define X_BED_SIZE 220
+		    #define Y_BED_SIZE 220
+		    #define Z_MAX_POS 300
+	   	#else
+			#define X_BED_SIZE 235
+			#define Y_BED_SIZE 235
+			#define Z_MAX_POS 250
+		#endif
   #endif
 
   #if ENABLED(HOME_ADJUST)
