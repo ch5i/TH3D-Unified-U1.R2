6b5624d983f1a9188d549d9c70ceb33accb1ffa6
Thomas <ch5i@users.noreply.github.com>
2020-05-02 11:49:00 +0200

    Updated Ender5 bedsize

--- TH3DUF_R2/Configuration_backend.h
+++ TH3DUF_R2/Configuration_backend.h
@@ -2327,13 +2327,8 @@
   #if ENABLED(ENDER5)
     #if ENABLED(BLTOUCH)
       #define X_BED_SIZE 220
-      #define Y_BED_SIZE 235
+      #define Y_BED_SIZE 220
       #define Z_MAX_POS 300
-    #else
-      #define X_BED_SIZE 235
-      #define Y_BED_SIZE 235
-      #define Z_MAX_POS 300
-    #endif
   #endif
 
   #if ENABLED(HOME_ADJUST)
